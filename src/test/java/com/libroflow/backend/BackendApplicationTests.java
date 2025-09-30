10648d7e589520342383d46c3e171a78439d1213
Motasem Alatawna <mutasim.alatawneh@gmail.com>
2025-09-30 14:50:41 +0300

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/libroflow/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libroflow.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
